9bae3a25187a09a9b3d761750badb85682cc32d0
abarraghi <abarraghi@gmail.com>
2024-03-17 08:30:36 -0400

    Added Maps viewing for an event by string

--- src/app/event/[id]/page.tsx
+++ src/app/event/[id]/page.tsx
@@ -1,7 +1,28 @@
+"use client";
+
+import { useEffect } from "react";
+
 export default function Page({ params }: { params: { id: string } }) {
   const id = parseInt(params.id);
   if (!Number.isSafeInteger(id) || id < 0) {
     return <p>Invalid Event ID</p>;
   }
-  return <p>Post: {id}</p>
+
+  var map = <iframe id="map-view"></iframe>
+
+  function displayMap(address: string){
+
+    useEffect(() => {
+      const link = `https://www.google.com/maps?q=${address}&output=embed`
+
+      document.getElementById("map-view")!.setAttribute("src", link);
+    },
+    []
+    );
+
+  }
+
+  displayMap("Penn State Business Building")
+
+  return map
 }
