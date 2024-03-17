27f5ed53dbdd2177c182d89bad73e27f5a8f9ffa
Kristopher Hicks <kristopher.t.hicks@gmail.com>
2024-03-17 04:21:43 -0400

    Flexbox goated

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -24,15 +24,18 @@ export default function Dashboard() {
   }, []);
 
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <div className="flex w-full p-4 items-center bg-slate-200">
         <h1>Dashboard</h1>
         <div className="grow self-stretch"></div>
         <SignOutButton className="self-baseline"></SignOutButton>
       </div>
-      {loading && <p>Loading...</p>}
+      {loading &&
+        <div className="flex flex-col self-stretch grow p-8">
+          <p>Loading...</p>
+        </div>}
       {!loading && !error &&
-        <div className="flex flex-col p-8">
+        <div className="flex flex-col self-stretch grow p-8">
           Dashboard content
         </div>}
     </div>
