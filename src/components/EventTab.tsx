92d01794fa13009cb7cb06ed7dad1ab1a71b8bd5
Kristopher Hicks <kristopher.t.hicks@gmail.com>
2024-03-17 09:57:39 -0400

    Fixed stuff

--- src/components/EventTab.tsx
+++ src/components/EventTab.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function EventTab(props: { name: string, id: number, created_at: Date }) {
 
-  function timeSince(date:Date){
+  function timeSince(date: Date) {
     let seconds = Math.floor((new Date().getTime() - date.getTime()) / 1000);
     let interval = seconds / 31536000;
 
@@ -17,13 +17,11 @@ export default function EventTab(props: { name: string, id: number, created_at: 
     return Math.floor(seconds) + " seconds ago";
   }
 
-  
+
   let eventDate = new Date(props.created_at);
 
-  console.log(timeSince(eventDate));
-  
-  return <div className="flex justify-between p-6 hover:py-9 truncate hover:bg-gray-200 duration-300 transition-all bg-gray-100">
-    <Link href='/'>{props.name}</Link>
+  return <Link href={`/event/${props.id}`}><div className="flex justify-between p-6 hover:py-9 truncate hover:bg-gray-200 duration-300 transition-all bg-gray-100">
+    <div>{props.name}</div>
     <p className="text-neutral-500">{timeSince(eventDate)}</p>
-    </div>;
+  </div></Link>;
 }
