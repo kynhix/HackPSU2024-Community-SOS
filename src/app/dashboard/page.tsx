067cbbb23d7d575d595d4d7e3e0a1318a9419da1
Kristopher Hicks <kristopher.t.hicks@gmail.com>
2024-03-17 10:28:19 -0400

    Added loading

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -31,23 +31,20 @@ export default function Dashboard() {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar></Navbar>
-      {loading &&
-        <div className="flex flex-col self-stretch grow p-8">
-          <p>Loading...</p>
-        </div>}
-      {!loading && !error &&
-        <div className="flex flex-col items-center self-stretch grow p-8">
-          <div className="flex flex-col rounded-lg overflow-hidden w-full shadow-md max-w-[1440px]">
-            <div className="flex justify-between items-center bg-slate-500">
-              <div className="p-6 font-bold bg-slate-500 text-slate-50">Events</div>
-              <Link href="/form" className="mr-2 rounded-lg h-fit bg-white transition-colors hover:text-white hover:bg-blue-500 p-4" type="submit">Create Event</Link>
-            </div>
-            {events.map((e) =>
-              // @ts-ignore
-              <EventTab key={e.id} name={e.name} id={e.id} created_at={e.created_at}></EventTab>
-            )}
+      <div className="flex flex-col items-center self-stretch grow p-8">
+        <div className="flex flex-col rounded-lg overflow-hidden w-full shadow-md max-w-[1440px]">
+          <div className="flex justify-between items-center bg-slate-500">
+            <div className="p-6 font-bold bg-slate-500 text-slate-50">Events</div>
+            <Link href="/form" className="mr-2 rounded-lg h-fit bg-white transition-colors hover:text-white hover:bg-blue-500 p-4" type="submit">Create Event</Link>
           </div>
-        </div>}
+          {!loading && !error && events.map((e) =>
+            // @ts-ignore
+            <EventTab key={e.id} name={e.name} id={e.id} created_at={e.created_at}></EventTab>
+          )}
+          {loading && <p className="p-8">Loading...</p>}
+          {error && <p>Error fetching data</p>}
+        </div>
+      </div>
     </div>
   );
 }
